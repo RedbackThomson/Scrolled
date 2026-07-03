231fd60899d28078311e325a95f160f248efef17
Nicholas Thomson <RedbackThomson@users.noreply.github.com>
2026-07-03 15:24:13 -0700

    Fix command palette styling

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -3,7 +3,11 @@ import preset from '@scrolled/ui/tailwind-preset';
 
 const config: Config = {
   presets: [preset],
-  content: ['./index.html', './src/**/*.{ts,tsx}'],
+  content: [
+    './index.html',
+    './src/**/*.{ts,tsx}',
+    '../../packages/ui/src/**/*.{ts,tsx}',
+  ],
 };
 
 export default config;
